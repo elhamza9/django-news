fc3715e5681432d2c929e3c42948a0935f2da9b6
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-09 12:11:16 +0100

    Add Topic Model

--- djangonews/topics/admin.py
+++ djangonews/topics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Topic, TopicAdmin)
--- djangonews/topics/models.py
+++ djangonews/topics/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    pub_date = models.DateTimeField(blank=False, null=False)
+    title = models.CharField(blank=False, null=False, max_length=100)
+    slug =  models.CharField(blank=False, null=False, max_length=100)
+    content = models.TextField(blank=False, null=False)
+
+    class Meta:
+        db_table = 'topics'
+
+    def __str__(self):
+        return self.title
