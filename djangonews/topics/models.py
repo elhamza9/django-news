d486adf8989d999ceb1ee99592c1d846250b0c5f
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-10 08:29:36 +0100

    Create list_topics Endpoint

--- djangonews/topics/models.py
+++ djangonews/topics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    published_at = models.DateTimeField(blank=False, null=False)
+    title = models.CharField(blank=False, null=False, max_length=100)
+    slug = models.CharField(blank=False, null=False, max_length=100)
+    content = models.TextField(blank=False, null=False)
+    
+    class Meta:
+        db_table = 'topics'
+        ordering = ['published_at']
+
+    def __str__(self):
+        return self.title
--- djangonews/topics/views.py
+++ djangonews/topics/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Topic
 
 # Create your views here.
+
+def list_topics(request):
+    topics = Topic.objects.all()
+    titles = []
+    for t in topics:
+        titles.append(t.title)
+    return HttpResponse('Here I list all topics: %s' % (' - '.join(titles)))
