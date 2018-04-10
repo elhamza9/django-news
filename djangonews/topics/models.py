ab3054f54a93c4883c5e4cb44ccbb5187ec0255d
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-10 11:44:28 +0100

    Add comment model and author for Topic and Comment

--- djangonews/topics/models.py
+++ djangonews/topics/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Topic(models.Model):
     title = models.CharField(blank=False, null=False, max_length=100)
     slug = models.CharField(blank=False, null=False, max_length=100)
     content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='author of the Topic')
     
     class Meta:
         db_table = 'topics'
@@ -14,3 +16,16 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    published_at = models.DateTimeField(blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='comments', verbose_name='related Topic')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='author of the comment')
+
+    class Meta:
+        db_table = 'comments'
+
+    def __str__(self):
+        return self.content[:10]
