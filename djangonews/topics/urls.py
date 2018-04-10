02350d7be265222251b575c1bc18b592832b4002
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-10 10:59:19 +0100

    Add detail topic endpoint

--- djangonews/topics/urls.py
+++ djangonews/topics/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_topics, name='list_topics')
+    path('', views.list_topics, name='list_topics'),
+    path('<str:slug>', views.detail_topic, name='detail_topic')
 ]
--- djangonews/topics/views.py
+++ djangonews/topics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Topic
 
@@ -14,3 +14,17 @@ def list_topics(request):
         titles.append(t.title)
     return HttpResponse('Here I list all topics: %s' % (' - '.join(titles)))
     '''
+
+def detail_topic(request, slug=''):
+    try:
+        assert len(slug) > 0
+        res = Topic.objects.filter(slug=slug)
+        assert len(res) == 1
+    except AssertionError:
+        raise Http404("Article Not Found !")
+
+    topic = res[0]
+    return render(request, 'topics/detail.html',{
+            'title': topic.title, 
+            'published_at': topic.published_at ,
+            'content': topic.content})
