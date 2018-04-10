51e733a27da05541ad6d926a6c5e99908a1933d6
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-10 11:50:16 +0100

    Add Comment to Admin panel

--- djangonews/topics/admin.py
+++ djangonews/topics/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Comment
 
 # Register your models here.
 
 class TopicAdmin(admin.ModelAdmin):
     pass
 
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Topic, TopicAdmin)
+admin.site.register(Comment, CommentAdmin)
