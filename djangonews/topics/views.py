8e76db9d4ef99164cc0f077470a92dddea39851c
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-12 12:12:08 +0100

    Check logged user in deleting comment

--- djangonews/topics/views.py
+++ djangonews/topics/views.py
@@ -75,7 +75,7 @@ def submit_comment(request, id_topic=0):
 
 def delete_comment(request, id_comment=0):
     comment = get_object_or_404(Comment, pk=id_comment)
-
+    assert comment.author == request.user
     comment.delete()
     return redirect('detail_topic', slug=comment.topic.slug)
 
