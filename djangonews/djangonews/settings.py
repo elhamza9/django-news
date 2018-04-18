5cc7e79273cfd18f052bbb995f410edd1a40483f
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-18 15:09:44 +0100

    Get Allowed Host from env variable

--- djangonews/djangonews/settings.py
+++ djangonews/djangonews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.environ.get('USED_DOMAIN')]
 
 
 # Application definition
