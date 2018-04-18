be892752c042783a30d5a8179c68d28ce0a82f62
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-18 14:09:37 +0100

    Change sqlite file to a folder

--- djangonews/djangonews/settings.py
+++ djangonews/djangonews/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'djangonews.wsgi.application'
 DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
 #        'ENGINE': 'django.db.backends.mysql',
 #        'NAME': 'djangonews_db',
 #        'USER': 'django',
