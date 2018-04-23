41d8d26255dea56f6e5bb9ac7a63d4b1abb0d821
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-23 10:10:25 +0100

    Make it deployment ready

--- djangonews/entry.sh
+++ djangonews/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#python3 manage.py makemigrations
+sleep 5 # quick fix to wait for DB to start
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py collectstatic
+gunicorn --access-logfile - --log-file - --workers 2 djangonews.wsgi:application -b 0.0.0.0:8000
