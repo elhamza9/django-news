1b63694a3c157c552f04e0077e43292dd95ea79d
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-24 17:30:09 +0100

    Fix: make slug unique

--- djangonews/topics/models.py
+++ djangonews/topics/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class Topic(models.Model):
     published_at = models.DateTimeField(blank=False, null=False,  default=timezone.now())
     title = models.CharField(blank=False, null=False, max_length=100)
-    slug = models.CharField(blank=False, null=False, max_length=100)
+    slug = models.CharField(blank=False, null=False, unique=True,max_length=100)
     content = models.TextField(blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='author of the Topic')
     nbr_upvotes = models.IntegerField(blank=False, null=False, default=0)
