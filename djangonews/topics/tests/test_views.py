c7e3b04df819c7b488a29e1afc90c5ce81262cb1
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-26 15:04:24 +0100

    Refactor code

--- djangonews/topics/tests/test_views.py
+++ djangonews/topics/tests/test_views.py
@@ -13,44 +13,53 @@ from topics.models import Topic, Comment, Upvote
 
 pytestmark = pytest.mark.django_db
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def req_factory():
     return RequestFactory()
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client():
     return Client()
 
+plain_pass = 'mysecurepass'
+@pytest.fixture
+def registered_user():
+    return User.objects.create_user(username='dummy', password=plain_pass)
+
+@pytest.fixture(scope="module")
+def anonymous_user():
+    return AnonymousUser()
 
 
 class TestTopic:
 
-    def test_list_topics_view(self, req_factory):
+    def test_list_topics_view(self, req_factory, anonymous_user):
         '''
             Test list_topic view
         '''
         req = req_factory.get(reverse('site_index'))
+        req.user = anonymous_user
         resp = views.list_topics(req)
         assert resp.status_code == 200
 
-    def test_detail_topic(self, req_factory):
+    def test_detail_topic_view(self, req_factory, anonymous_user):
         '''
             Test Anonymous user access Detail Topic View
         '''
         t = mommy.make('topics.Topic')
         req = req_factory.get(reverse('detail_topic',kwargs={'slug':t.slug}))
-        req.user = AnonymousUser()
+        req.user = anonymous_user
         resp = views.detail_topic(req, t.slug)
         assert resp.status_code == 200 , 'View Should return 200'
 
-    def test_detail_non_existing_topic_raises_exception(self,req_factory):
+    def test_detail_topic_wrong_slug_raises_exception(self,req_factory, anonymous_user):
         '''
             Test Http404 Exception gets raised when trying to access
             page of non existing topic ( slug part is wrong )
         '''
         wrong_slug = 'some-wrong-slug'
         req = req_factory.get(reverse('detail_topic',kwargs={'slug':wrong_slug}))
-        req.user = AnonymousUser()
+        req.user = anonymous_user
         with pytest.raises(Http404):
             resp = views.detail_topic(req, wrong_slug)
 
@@ -61,79 +70,72 @@ class TestTopic:
         resp = client.get(reverse('add_topic'), follow=True)
         last_url, code = resp.redirect_chain[-1]
         assert last_url == reverse('user_login')
+        assert code == 302
 
-    def test_add_topic_view_authenticated(self, req_factory):
+    def test_logged_can_see_add_topic_view(self, req_factory, registered_user):
         '''
             Test that authenticated user can access add_topic view
         '''
         req = req_factory.get(reverse('add_topic'))
-        req.user = mommy.make('User')
+        req.user = registered_user
         resp = views.add_topic(req)
         assert resp.status_code == 200, 'View should return 200 because user is authenticated'
 
-    def test_logged_can_post_topic(self, req_factory):
+    def test_logged_can_post_topic(self, req_factory, registered_user):
         '''
             Test that a logged user can add a topic
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy1', password=plain_pass)
         req = req_factory.post(reverse('add_topic'), data={
             'title': 'My Topic',
             'slug': 'my-topic',
             'content': 'My Awesome topic is great. upvote it !'
         })
-        req.user = u
+        req.user = registered_user
         resp = views.add_topic(req)
         assert Topic.objects.get(slug='my-topic')
-    
-    def test_logged_post_invalid_topic_exception(self, req_factory):
+
+    def test_logged_post_invalid_topic_exception(self, req_factory, registered_user):
         '''
             Test that an exception get raised when a  logged user
             tries to submit a invalid form to add a topic
             (it should actually be prevented by javascript, but
             just in case)
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy1', password=plain_pass)
         req = req_factory.post(reverse('add_topic'), data={
             'title': 'My Topic',
             'slug': '', # No slug !
             'content': 'My Awesome topic is great. upvote it !'
         })
-        req.user = u
+        req.user = registered_user
         with pytest.raises(Http404):
             resp = views.add_topic(req)
 
 class TestComment:
 
-    def test_logged_user_submit_comment(self, req_factory):
+    def test_logged_can_submit_comment(self, req_factory, registered_user):
         '''
             Test that a logged user can post a comment on a Topic
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy1', password=plain_pass)
         t = mommy.make('topics.Topic')
         req = req_factory.post(reverse('submit_comment', kwargs={'id_topic': t.id}),
                 data= {'content': 'My Comment'})
-        req.user = u
+        req.user = registered_user
         resp = views.submit_comment(req, t.id)
-        assert Comment.objects.get(author=u)
+        assert Comment.objects.get(author=registered_user)
 
-    def test_user_cant_submit_empty_comment(self, req_factory):
+    def test_logged_cant_submit_empty_comment(self, req_factory, registered_user):
         '''
             Test that a user cant post an empty comment on a Topic
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy2', password=plain_pass)
         t = mommy.make('topics.Topic')
         req = req_factory.post(reverse('submit_comment', kwargs={'id_topic': t.id}),
                 data= {'content': ''})
-        req.user = u
+        req.user = registered_user
         with pytest.raises(Http404):
             resp = views.submit_comment(req, t.id)
 
 
-    def test_anonymous_comment_is_redirected_to_login(self, client):
+    def test_anonymous_submit_comment_redirected_to_login(self, client):
         '''
             Test that anonymous user can't access add_topic view
         '''
@@ -141,55 +143,52 @@ class TestComment:
         last_url, code = resp.redirect_chain[-1]
         assert last_url == reverse('user_login')
 
-    def test_user_can_delete_his_comment(self, req_factory):
+    def test_logged_can_delete_his_comment(self, req_factory, registered_user):
         '''
             Test that user can delete his own comment
         '''
-        u = mommy.make('User')
-        c = mommy.make('topics.Comment', author=u)
+        c = mommy.make('topics.Comment', author=registered_user)
         req = req_factory.get(reverse('delete_comment', kwargs={'id_comment': c.id}))
-        req.user = u
+        req.user = registered_user
         resp = views.delete_comment(req, c.id)
         #assert resp.status_code == 200
         with pytest.raises(ObjectDoesNotExist):
             Comment.objects.get(id=c.id)
 
-    def test_user_cant_delete_comment_of_another_user(self, req_factory):
+    def test_logged_cant_delete_comment_of_another_user(self, req_factory, registered_user):
         '''
             Test that 404 Exception gets raised when
             user tries to delete another one's comment
         '''
-        users = mommy.make('User', _quantity=2)
-        c = mommy.make('topics.Comment', author=users[0])
+        other_user = mommy.make('User')
+        c = mommy.make('topics.Comment', author=other_user)
         req = req_factory.get(reverse('delete_comment', kwargs={'id_comment': c.id}))
-        req.user = users[1]
+        req.user = registered_user
         with pytest.raises(Http404):
             resp = views.delete_comment(req, c.id)
 
-    def test_anonymous_cant_delete_comment(self, req_factory):
+    def test_anonymous_cant_delete_comment(self, req_factory, anonymous_user):
         '''
             Test that 404 Exception gets raised when
             anonymous user tries to delete a comment
         '''
         c = mommy.make('topics.Comment')
         req = req_factory.get(reverse('delete_comment', kwargs={'id_comment': c.id}))
-        req.user = AnonymousUser()
+        req.user = anonymous_user
         with pytest.raises(Http404):
             resp = views.delete_comment(req, c.id)
 
 class TestUpvote:
 
-    def test_logged_user_can_upvote_topic(self, req_factory):
+    def test_logged_can_upvote_topic(self, req_factory, registered_user):
         '''
             Test that a logged user can upvote a topic
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy2', password=plain_pass)
         t = mommy.make('topics.Topic')
         req = req_factory.post(reverse('upvote_topic', kwargs={'id_topic': t.id}), data={})
-        req.user = u
+        req.user = registered_user
         resp = views.upvote_topic(req, t.id)
-        assert Upvote.objects.get(upvoter=u)
+        assert Upvote.objects.get(upvoter=registered_user)
 
     def test_anonymous_upvote_is_redirected_to_login(self, client):
         '''
@@ -201,42 +200,40 @@ class TestUpvote:
         assert code == 302
         assert last_url == reverse('user_login')
 
-    def test_redirect_when_user_tries_to_upvote_two_times(self, client):
+    def test_redirect_when_logged_upvotes_or_cancels_upvote(self, client, registered_user):
         '''
             Test that authenticated user will be redirected to topic page
             when he/she tries to upvote topic more than one time
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy2', password=plain_pass)
         t = mommy.make('topics.Topic')
-        logged = client.login(username=u.get_username(), password=plain_pass)
+        logged = client.login(username=registered_user.get_username(), password=plain_pass)
         assert logged == True
-        resp = client.post(reverse('upvote_topic', kwargs={'id_topic': t.id}), follow=True)
-        assert resp.status_code == 200
         resp = client.post(reverse('upvote_topic', kwargs={'id_topic': t.id}), follow=True)
         last_url, code = resp.redirect_chain[-1]
         assert code == 302
         assert last_url == reverse('detail_topic',kwargs={'slug': t.slug})
+        resp = client.post(reverse('cancel_upvote_topic', kwargs={'id_topic': t.id}), follow=True)
+        last_url, code = resp.redirect_chain[-1]
+        assert code == 302
+        assert last_url == reverse('detail_topic',kwargs={'slug': t.slug})
 
-    def test_logged_user_can_cancel_upvote(self, req_factory):
+    def test_logged_user_can_cancel_upvote(self, req_factory, registered_user):
         '''
             Test that logged user can cancel his upvote
             his upvote gets deleted from DB
         '''
-        plain_pass = 'mysecurepass'
-        u = User.objects.create_user(username='dummy2', password=plain_pass)
         t = mommy.make('topics.Topic')
         req = req_factory.post(reverse('upvote_topic', kwargs={'id_topic': t.id}), data={})
-        req.user = u
+        req.user = registered_user
         resp = views.upvote_topic(req, t.id)
-        assert Upvote.objects.get(upvoter=u)
+        assert Upvote.objects.get(upvoter=registered_user)
         req = req_factory.post(reverse('cancel_upvote_topic', kwargs={'id_topic': t.id}), data={})
-        req.user = u
+        req.user = registered_user
         resp = views.upvote_topic_cancel(req, t.id)
         with pytest.raises(ObjectDoesNotExist):
-            assert Upvote.objects.get(upvoter=u)
+            assert Upvote.objects.get(upvoter=registered_user)
 
-    def test_anonymous_cancel_upvote_is_redirected_to_login(self, client):
+    def test_anonymous_cancel_upvote_redirected_to_login(self, client):
         '''
             Test Anonymous user can't cancel upvote
         '''
