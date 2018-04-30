dad580c4e699daf84dd0c125a2c8fc61033f2700
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-30 15:50:40 +0100

    Add prod environment variable

--- djangonews/djangonews/settings.py
+++ djangonews/djangonews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or 'obg%w36jj!v+in^vq^a0&359*uxnq9*_(2^1-+q%90yy-lbgqh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('ENV') == 'debug' else False
+DEBUG = True if os.environ.get('ENV') != 'prod' else False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.environ.get('USED_DOMAIN')]
 
