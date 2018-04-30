11b2588ca957f50638c5a9007a974c003c6f5919
Hamza El Bouatmani <hamza@mon-pc.local>
2018-04-30 21:41:47 +0100

    Change Dockerfile

--- djangonews/entry.sh
+++ djangonews/entry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+sleep 5 && \
+python3 manage.py migrate && \
+gunicorn --access-logfile - --log-file - --workers 2 djangonews.wsgi:application -b 0.0.0.0:8000
